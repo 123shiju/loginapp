6c5ccf2857267e37126a2cfb94229d59c0c15e93
shiju k <106902741+123shiju@users.noreply.github.com>
2024-08-08 11:07:29 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -12,7 +12,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://loginapp-client.vercel.app/",
     methods:['POST','GET','PUT','DELETE'],
     credential:true
   })
